d293b9d1b5bf08e090100041b58e9ecad84a2a6e
CERSKF <luca.cerato@gmail.com>
2020-04-18 17:57:50 +0200

    e

--- prova2.js
+++ prova2.js
@@ -1,13 +1,26 @@
 
 
 
-var nome = window.prompt("Inserisci il tuo nome");
-if (nome != null) {
-  // ...
-}
+//var nome = window.prompt("Inserisci il tuo nome");
+//if (nome != null) {
+
+//}
 
 
 document.write(nome);
 
-x=www;
-document.write(x);
+o=www;
+document.write(o);
+
+
+var x = 123;
+function somma(a, b) {
+	return a + b;
+}
+
+console.log(x);
+console.log(somma(3, 5));
+console.log(window.x);
+console.log(window.somma(3, 5));
+console.log(this.x);
+console.log(this.somma(3, 5));
